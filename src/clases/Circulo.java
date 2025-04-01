3ace4e0dd8338f1e0eaa7e9fc25fc4b9bf6f972a
Atari96 <pmorato10@yahoo.es>
2025-04-01 16:24:11 +0200

    Primer commit del proyecto

---
+++ src/clases/Circulo.java
@@ -0,0 +1,49 @@
+package clases;
+
+public class Circulo {
+	private int id;
+	private String nombre;
+	private double radio;
+	
+	public Circulo() {}
+	
+	public Circulo(int id, String nombre, double radio) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.radio = radio;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getRadio() {
+		return radio;
+	}
+
+	public void setRadio(double radio) {
+		this.radio = radio;
+	}
+
+	@Override
+	public String toString() {
+		return "Circulo [id=" + id + ", nombre=" + nombre + ", radio=" + radio + "]";
+	}
+	
+	
+	
+	
+}
